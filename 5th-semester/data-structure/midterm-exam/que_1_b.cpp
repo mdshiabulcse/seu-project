1fc11669e0cf99f85d2b1930a4a575b61426111c
shiabulislam <md.shiabul.cse@gmail.com>
2025-01-06 09:54:13 +0600

    midterm question assignment one complete

--- 5th-semester/data-structure/midterm-exam/que_1_b.cpp
+++ 5th-semester/data-structure/midterm-exam/que_1_b.cpp
@@ -4,6 +4,7 @@
 
 #include "que_1_b.h"
 #include <iostream>
+#include <string>
 using namespace std;
 
 
@@ -11,21 +12,15 @@ struct Node{
     int a;
     float b;
     char c;
-    string name;
     Node* pre;
     Node* next;
 };
 
-Node* createNode(int a, float b, char c, string name){
-    Node* newNode=(Node*) malloc(sizeof(Node));
-    if (newNode == nullptr){
-        cout << "Invalid Data" << endl;
-        exit(1);
-    }
+Node* createNode(int a, float b, char c){
+    Node* newNode=new Node();
     newNode->a=a;
     newNode->b=b;
     newNode->c=c;
-    newNode->name=name;
     newNode->pre= nullptr;
     newNode->next= nullptr;
     return newNode;
@@ -34,6 +29,43 @@ Node* createNode(int a, float b, char c, string name){
 void displayList(Node* head){
     Node* data=head;
     while (data != nullptr){
-        cout << "["<<data->a<<","<<data->b
+        cout << "["<<data->a<<","<<data->b<<","<<data->c<<"]<->";
+        data=data->next;
     }
+    cout<<"Null"<<endl;
+
+}
+void insertAfter(Node* prevNode, int a, float b, char c){
+    if (prevNode == nullptr){
+        cout<<"Previous node can not be null"<<endl;
+        return;
+    }
+    Node* newNode= createNode(a,b,c);
+    newNode->next=prevNode->next;
+    prevNode->next=newNode;
+
+    newNode->pre=prevNode;
+    if (newNode->next != nullptr){
+        newNode->next->pre=newNode;
+    }
+
+}
+
+int main(){
+    Node* head= createNode(1,3.5,'X');
+    Node* third= createNode(3,3.7,'Z');
+
+    head->next=third;
+    third->pre=head;
+
+
+
+    cout <<"Main doubly linked List:"<<endl;
+    displayList(head);
+    cout << "\nInserting new node (2, 4.00, 'Y') after the first node:" << endl;
+    insertAfter(head, 2, 4.00, 'Y');
+
+    displayList(head);
+
+    return 0;
 }
